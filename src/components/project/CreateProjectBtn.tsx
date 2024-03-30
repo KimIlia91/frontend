8be58537332331bc23c56d4a94e62a33dd01bfb5
paterfamelias@gmail.com <paterfamelias@gmail.com>
2024-03-31 00:11:57 +0600

    added

--- src/components/project/CreateProjectBtn.tsx
+++ src/components/project/CreateProjectBtn.tsx
@@ -2,18 +2,30 @@
 
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react';
 
 const CreateProjectBtn = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
   const onClick = () => {
     console.log('Click')
   }
 
   return (
-    <Button className='mb-10' onClick={onClick}>
-      <Link href='/project/create'>
-        Добавить проект
-      </Link>
-    </Button>
+    <>
+      {/* <Button className='mb-10' onClick={onClick}>
+          Добавить проект
+      </Button> */}
+      <Button className='mb-10' onClick={onClick}>
+        <Link href='/project/create'>
+          Добавить проект
+        </Link>
+      </Button>
+      <div className={`w-[200px] h-[200px] bg-white ${isOpenModal ? 'block' : 'hidden'}`}>
+        modal
+      </div>
+    </>
+    
   )
 }
 
